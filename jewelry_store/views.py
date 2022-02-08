9b7b83bd9dfba6834d46932fb3e51a1d36c53de6
hebel-dev <hebel.dev@gmail.com>
2022-02-08 15:24:27 +0100

    building category structure in html header and views to display dropdown item

--- jewelry_store/views.py
+++ jewelry_store/views.py
@@ -7,3 +7,8 @@ from .models import Category, Product
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'jewelry_store/home.html', {'products':products})
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
