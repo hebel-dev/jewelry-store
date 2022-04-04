5d0e58afa7397464b8c16a2481ac0066972deb69
hebel-dev <hebel.dev@gmail.com>
2022-04-04 19:54:36 +0200

    Adding id to giving js identifire to colect data from

--- basket/basket.py
+++ basket/basket.py
@@ -8,6 +8,6 @@ class Basket():
         #every sesion is request
         self.session = request.session
         basket = self.session.get('session_key')
-        if 'session_key' not in request.sesion:
+        if 'session_key' not in request.session:
             basket = self.session['session_key'] = {}
         self.basket = basket
