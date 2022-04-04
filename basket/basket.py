cdb258c0a6fda11e57b3d521f5358371e83b7191
hebel-dev <hebel.dev@gmail.com>
2022-04-04 19:24:37 +0200

    Create class Basket and context processors

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,13 @@
 
 
 
+class Basket():
+
+    def __init__(self, request):
+        # if sesion dosent exist the session will be created
+        #every sesion is request
+        self.session = request.session
+        basket = self.session.get('session_key')
+        if 'session_key' not in request.sesion:
+            basket = self.session['session_key'] = {}
+        self.basket = basket
